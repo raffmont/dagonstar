14e37c824d554ee6f9b45a071231ead7ca9a6851
ddomizzisg <1330043@upv.edu.mx>
2018-10-04 17:15:28 +0200

    First version of dagon with docker

---
+++ examples/dataflow-demo-docker.py
@@ -0,0 +1,46 @@
+from dagon import Workflow
+from dagon import batch
+from dagon import docker_task as dt
+import json
+import sys
+import datetime
+import os.path
+
+# Check if this is the main
+if __name__ == '__main__':
+
+  config={
+    "scratch_dir_base":"test/",
+    "remove_dir":False
+  }
+
+  # Create the orchestration workflow
+  workflow=Workflow("DataFlow-Demo",config)
+  
+  # The task a
+  taskA=batch.Batch("Tokio","ls > f1.txt")
+  
+  # The task b
+  taskB=batch.Batch("Berlin","echo Soy Berlin > f2.txt; cat workflow://Tokio/f1.txt >> f2.txt")
+  
+  # The task c
+  taskC=batch.Batch("Nairobi","echo $RANDOM > f2.txt; cat workflow://Tokio/f1.txt >> f2.txt")
+  
+  # The task d
+  taskD=batch.Batch("Mosco","cat workflow://Berlin/f2.txt workflow://Nairobi/f2.txt > f3.txt")
+  
+  # add tasks to the workflow
+  workflow.add_task(taskA)
+  workflow.add_task(taskB)
+  workflow.add_task(taskC)
+  workflow.add_task(taskD)
+
+  workflow.make_dependencies()
+
+  jsonWorkflow=workflow.asJson()
+  with open('dataflow-demo-docker.json', 'w') as outfile:
+    stringWorkflow=json.dumps(jsonWorkflow,sort_keys=True, indent=2)
+    outfile.write(stringWorkflow)
+ 
+  # run the workflow
+  workflow.run()
