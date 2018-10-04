adba02105443665c95c0f4e9d1e769b2d3252b09
raffmont <raffaele.montella@uniparthenope.it>
2018-10-04 10:12:53 +0200

    New file

---
+++ examples/dataflow-demo.py
@@ -0,0 +1,45 @@
+from yawe import Workflow
+from yawe import batch
+import json
+import sys
+import datetime
+import os.path
+
+# Check if this is the main
+if __name__ == '__main__':
+
+  config={
+    "scratch_dir_base":"/home/ccmmma/dev/yawe/tmp/",
+    "remove_dir":False
+  }
+
+  # Create the orchestration workflow
+  workflow=Workflow("DiamondDemo",config)
+  
+  # The task a
+  taskA=batch.Batch("A","echo $RANDOM > f1.txt")
+  
+  # The task b
+  taskB=batch.Batch("B","echo $RANDOM > f2.txt; cat workflow://A/f1.txt >> f2.txt")
+  
+  # The task c
+  taskC=batch.Batch("C","echo $RANDOM > f2.txt; cat workflow://A/f1.txt >> f2.txt")
+  
+  # The task d
+  taskD=batch.Batch("D","cat workflow://B/f2.txt workflow://D/f2.txt > f3.txt")
+  
+  # add tasks to the workflow
+  workflow.add_task(taskA)
+  workflow.add_task(taskB)
+  workflow.add_task(taskC)
+  workflow.add_task(taskD)
+
+  workflow.make_dependencies()
+
+  jsonWorkflow=workflow.asJson()
+  with open('diamond.json', 'w') as outfile:
+    stringWorkflow=json.dumps(jsonWorkflow,sort_keys=True, indent=2)
+    outfile.write(stringWorkflow)
+ 
+  # run the workflow
+  workflow.run()
